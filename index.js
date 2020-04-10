d2191859a0e36aec19009b525e4499b43028d5c1
FAUGERAS <kflmada@gmail.com>
2020-04-10 16:51:22 +0200

    debutProjet

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-$("#idelem").click(fonction)
+
+
+
+$("#result")
+ $.ajax({
+        // URL de la requête
+       url: "https://pokeapi.co/api/v2/berry/1",
+        // methode d'envoi (type de requère)
+      method: "GET",
+        // format de réponse attendu
+      dataType: "json",
+    })
+   .done(function( data ) {
+      if ( console && console.log ) {
+         console.log( "Sample of data:", data.slice( 0, 100 ) );
+        }
+     })
+}
+
+
